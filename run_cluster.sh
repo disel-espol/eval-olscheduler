c8cae9b3bc8093c51a7ede8d6baa5291443542ec
Ubuntu <ubuntu@ip-172-31-52-82.ec2.internal>
2018-07-31 23:00:26 +0000

    Update evaluation scripts

--- run_cluster.sh
+++ run_cluster.sh
@@ -4,9 +4,11 @@ TEST_CLUSTER=c0
 NUM_WORKERS=5
 ADMIN=/home/ubuntu/open-lambda/bin/admin
 
-SANDBOX='{"sandbox": "sock"}'
+SANDBOX='{"sandbox": "sock", "handler_cache_size": 0, "import_cache_size": 0, "cg_pool_size": 10, "sock_base_path": "/home/ubuntu/c0/base"}'
 REGISTRY_DIR='{"registry_dir": "/home/ubuntu/pipbench/handlers_100_5/handlers"}'
-PIP_MIRROR='{"pip_mirror": "http://34.198.251.213:9199/simple/"}'
+# PIP_MIRROR='{"pip_mirror": "http://18.211.241.59:9199/simple/"}'
+# PIP_MIRROR='{"pip_mirror": "http://18.211.241.59:9199/simple/ --trusted-host 18.211.241.59"}'
+PIP_MIRROR='{"pip_mirror": "http://localhost:9199/simple/"}'
 
 $ADMIN kill -cluster=$TEST_CLUSTER
 rm -r $TEST_CLUSTER
@@ -19,4 +21,6 @@ eval "$ADMIN setconf -cluster=$TEST_CLUSTER '$PIP_MIRROR'"
 $ADMIN workers -cluster=$TEST_CLUSTER -num-workers=$NUM_WORKERS -port=8081
 
 $ADMIN status -cluster=$TEST_CLUSTER
+
+chmod 777 -R $TEST_CLUSTER
 
